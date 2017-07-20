f3bdfe803b3fd1068bac1c1ea0d38edd958da7c4
Jihoon Kim <jihoon_kim@outlook.com>
2017-07-20 12:50:35 +0900

    crawler: expand more button is available

--- crawler.py
+++ crawler.py
@@ -5,7 +5,7 @@ Author: Jihoon Kim
 """
 
 from selenium import webdriver
-
+from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome('/home/jihoon_kim/.ChromeDriver/chromedriver')
 driver.implicitly_wait(3)
@@ -18,3 +18,10 @@ driver.find_element_by_id("global-nav-attractions").click()
 # Test Case for '경복궁'
 driver.find_element_by_class_name("typeahead_input").send_keys('경복궁')
 driver.find_element_by_id("SUBMIT_THINGS_TO_DO").click()
+
+# Extract Latest Review
+latest_review_id = driver.find_element_by_xpath("//div[@class='reviewSelector']").get_attribute("id")
+id_num = latest_review_id.split('_')[1]
+target_path_for_more_button = "\"" + "review_" + id_num + "\""
+xpath_more = "//*[@id=" + target_path_for_more_button + "]/div/div[2]/div/div[1]/div[3]/div/p/span"
+driver.find_element_by_xpath(xpath_more).click()
